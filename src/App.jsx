1dca33adadc1b56e970afe62dd3b1841c98333b1
lukman bolaji <bolajiakanji21@gmail.com>
2024-05-26 00:56:57 -0700

    fix cache bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,119 +3,79 @@ import './App.css'
 
 function App() {
   
-  const [text, setText] = useState ("");
-  const [color, setcolor] = useState ("");
+  const [quoteStore, setQuoteStore] = useState ("");
+  const [randomQuote, setRandomQuote] = useState ("");
+  const [randomColor, setRandomColor] = useState ("");
+  
 
   useEffect(
     () =>{
       fetchData()
       getRandomColor()
+      
     }, []
   )
-  async function updateQuote() {
-    // Fetch a random quote from the Quotable API
-    const response = await fetch("https://api.quotable.io/random");
-    const data = await response.json();
-    if (response.ok) {
-      // Update DOM elements
-      quote.textContent = data.content;
-      cite.textContent = data.author;
-    } else {
-      quote.textContent = "An error occured";
-      console.log(data);
-    }
-  }
-  const fetchPokemon = () => {
-    const searchInput = document
-      .getElementById("search-input")
-      .value.toLowerCase();
-    displayPokemon.style.display = "none";
-    const url = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${searchInput}`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        const pokemons = data;
-        pokemonTypes.textContent = "";
-        if (
-          searchInput === pokemons.name ||
-          searchInput === pokemons.id.toString()
-        ) {
-          displayPokemon.style.display = "block";
-          pokemonName.textContent =
-            pokemons.name.toUpperCase();
-          pokemonId.textContent = `#${pokemons.id}`;
-          pokemonWeight.textContent = `Weight:${pokemons.weight}`;
-          pokemonHeight.textContent = `Height:${pokemons.height}`;
-          pokemonPhoto.src = pokemons.sprites.front_default;
-          const pokemonsTypes = pokemons.types;
-          pokemonsTypes.forEach((type) => {
-            pokemonTypes.innerHTML += `<span class="types" id="types">${type.type.name}</span>`;
-          });
-          hp.textContent = `${pokemons.stats[0].base_stat}`;
-          attack.textContent = `${pokemons.stats[1].base_stat}`;
-          defense.textContent = `${pokemons.stats[2].base_stat}`;
-          specialAttack.textContent = `${pokemons.stats[3].base_stat}`;
-          specialDefense.textContent = `${pokemons.stats[4].base_stat}`;
-          speed.textContent = `${pokemons.stats[5].base_stat}`;
-        } else if (
-          searchInput !== pokemons.name ||
-          searchInput !== pokemons.id.toString()
-        ) {
-          alert("Pokémon not found");
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("Pokémon not found");
-      });
-  };
-  const fetchData = async (name)=>{
-    const enteredNameOrID = "https://api.quotable.io/random"
+  
+  let fetchData = async ()=>{
+    const enteredNameOrID = "https://api.quotable.io/quotes?limit=150"
     
     
-    try {
-        const res = await fetch(enteredNameOrID);
+   try {
+       const res = await fetch(enteredNameOrID);
         const data = await res.json();
-        console.log(data)
-        setText(data)
-        console.log("sgayddudddyaaud")
+        
+        let y= await data.results
+        setQuoteStore(y)
+        loo(y)
+        
+        
 
         
       } catch (err) {
-        console.log("fix your network")
-        alert('network issue')
         
+        alert(' Check your network issue')
         
         
       }
+      }
       
-}
+      
+      function loo(xc){
+      setRandomQuote(xc[Math.floor(Math.random() * xc.length)])
 
-const getRandomColor = () => {
+        
+
+      }
+  
+let getRandomColor = () => {
   const red = Math.floor(Math.random() * 128);
   const green = Math.floor(Math.random() * 128);
   const blue = Math.floor(Math.random() * 128);
 
-  setcolor(`rgb(${red}, ${green}, ${blue})`);
+  setRandomColor(`rgb(${red}, ${green}, ${blue})`);
 };
 
-const transition = "all 3s";
+const transition = "background-color 3s";
 
-
+if (randomQuote) {
   return (
     
-      <div id="quote-box" style={{backgroundColor: color, transition: transition }}>
-        <div id="text">{text.content}</div>
-        <div id="author">{text.author}</div>
+    
+      <div id="quote-box" style={{backgroundColor: randomColor, transition: transition }}>
+        <div id="text">{randomQuote.content}</div>
+        <div id="author">{randomQuote.author}</div>
         
 
         <a href='https:www.//twitter.com/intent/tweet'  id="tweet-quote">tweet-quote</a>
-        <button id="new-quote" onClick={()=> {fetchData(); getRandomColor()}}>new-quote</button>
+        <button id="new-quote" onClick={()=> {loo(quoteStore); getRandomColor()}}>new-quote</button>
       </div>
       
        
     
   )
+} 
 }
 
+
 export default App
+
